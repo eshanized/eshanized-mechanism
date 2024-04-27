0159896e62fb72b390b7a076223bca0ca449c874
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-27 14:40:42 +0530

    [rename] imp mods

--- show.bat
+++ show.bat
@@ -1,10 +1,10 @@
 @echo off
-python -c "import pyautogui as pg; pg.click(785, 17, duration=2)"
-python -c "import pyautogui as pg; pg.click(903, 64, duration=2)"
+python -c "import pyautogui as pag; pag.click(785, 17, duration=2)"
+python -c "import pyautogui as pag; pag.click(903, 64, duration=2)"
 start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
-python -c "import pyautogui as pg; pg.click(147, 489, duration=2)"
-python -c "import pyautogui as pg; pg.click(156, 552, duration=2)"
-python -c "import pyautogui as pg; pg.click(587, 14, duration=2)"
+python -c "import pyautogui as pag; pag.click(147, 489, duration=2)"
+python -c "import pyautogui as pag; pag.click(156, 552, duration=2)"
+python -c "import pyautogui as pag; pag.click(587, 14, duration=2)"
 echo ..........................................................
 echo .................E S H A N I Z E D........................
 echo ..........................................................
